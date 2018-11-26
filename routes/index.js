e99f6cd3637674516f43ba85c5b7886a66fe026c
LoxtonJS <1063462242@qq.com>
2018-11-26 21:57:47 +0800

    2018/11/26晚上2小时搭建基本骨架，理解各个文化的联系熟悉引擎的运用

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users.html', function(req, res) {
+  res.render('users');
+});
+
+router.get('/brand.html', function(req, res) {
+  res.render('brand');
+});
+
+router.get('/phone.html', function(req, res) {
+  res.render('phone');
+})
+
 module.exports = router;
